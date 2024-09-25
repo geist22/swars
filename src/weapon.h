44aacbd6caff3192600516e6a864657607775aa0
geist22 <107389189+geist22@users.noreply.github.com>
2024-09-25 03:36:53 +0200

    Update weapon.h

--- src/weapon.h
+++ src/weapon.h
@@ -108,7 +108,6 @@ struct WeaponDef {
   ubyte Category;
   ushort Cost;
   ushort Funding;
-  ushort FundingClassic;
   ubyte PercentPerDay;
 };
 
@@ -116,6 +115,7 @@ struct WeaponDef {
  */
 struct WeaponDefAdd {
   char Name[12];
+  short FundingClassic;
 };
 
 struct WeaponsFourPack {
@@ -125,6 +125,7 @@ struct WeaponsFourPack {
 #pragma pack()
 /******************************************************************************/
 extern struct WeaponDef weapon_defs[33];
+extern struct WeaponDefAdd weapon_defs_a[33];
 extern struct TbNamedEnum weapon_names[33];
 extern ubyte weapon_tech_level[33];
 extern short persuaded_person_weapons_sell_cost_permil;
