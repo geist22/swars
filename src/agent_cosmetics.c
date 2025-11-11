bed5561ba8ecc0ff749737b2b267843a1c8282e4
geist22 <107389189+geist22@users.noreply.github.com>
2025-11-11 19:16:42 +0100

    Updated header with new name

--- src/agent_cosmetics.c
+++ src/agent_cosmetics.c
@@ -1,5 +1,5 @@
 /******************************************************************************/
-// Syndicate Wars Port, source port of the classic strategy game from Bullfrog.
+// Syndicate Wars Fan Expansion, source port of the classic game from Bullfrog.
 /******************************************************************************/
 /** @file agent_cosmetics.c
  *     Ingame appearance of player agents loading from file.
