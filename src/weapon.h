490c7759b91cee8c8bdb762aab1d316eebc97b10
geist22 <107389189+geist22@users.noreply.github.com>
2025-11-21 01:13:14 +0100

    Classic research (#25)
    * Added classic mod research cost
    
    * Added classic weapon research cost
    
    * Update rules.ini
    
    * Update rules.c
    
    * Update research.h
    
    * typo
    
    * Update research.c
    
    * Update cybmod.h
    
    * Update cybmod.c
    
    * Update research.c
    
    * Update weapon.h
    
    * Update weapon.c
    
    * Update weapon.c
    
    * Update cybmod.h
    
    * Update cybmod.c
    
    * Update weapon.h
    
    * Update weapon.c
    
    * Update research.c
    
    * Update cybmod.c
    
    * Update weapon.c
    
    * Update weapon.c
    
    * Update research.c
    
    * Update rules.c
    
    * Update cybmod.h
    
    * Update research.h
    
    * Update research.c
    
    * Update research.c
    
    * Update feresearch.c
    
    * Update feresearch.c
    
    * Update feresearch.c
    
    * Update rules.ini
    
    * Update cybmod.h
    
    * More braces for ModDefAdd
    
    * Comment feresearch.c
    
    * Comment research.c

--- src/weapon.h
+++ src/weapon.h
@@ -140,6 +140,7 @@ struct WeaponDef {
  */
 struct WeaponDefAdd {
   char Name[12];
+  short FundingClassic;
 };
 
 struct WeaponsFourPack {
@@ -149,6 +150,7 @@ struct WeaponsFourPack {
 #pragma pack()
 /******************************************************************************/
 extern struct WeaponDef weapon_defs[33];
+extern struct WeaponDefAdd weapon_defs_a[33];
 extern struct TbNamedEnum weapon_names[33];
 extern ubyte weapon_tech_level[33];
 extern ubyte weapon_sound[32];
