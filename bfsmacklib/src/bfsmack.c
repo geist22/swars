9caae236b33af6a1db95d31d7b42f903deab7125
mefistotelis <mefistotelis@gmail.com>
2025-12-22 18:32:06 +0100

    bfsmacklib: Remade most of play_smk_direct()
    The new code is still disabled.

--- bfsmacklib/src/bfsmack.c
+++ bfsmacklib/src/bfsmack.c
@@ -19,12 +19,20 @@
 /******************************************************************************/
 #include "bfsmack.h"
 
+#include "bfkeybd.h"
+#include "bfmemut.h"
+#include "bfpalette.h"
 #include "bfscreen.h"
+#include "bfsvaribl.h"
+
+#include "smack2ail.h"
+
 /******************************************************************************/
 //SmackDrawCallback smack_draw_callback = NULL;
 void *(*smack_malloc)(int);
 void (*smack_free)(void *);
 
+extern ubyte byte_1E56DC[PALETTE_8b_SIZE];
 /******************************************************************************/
 
 void set_smack_malloc(void *(*cb)(int))
@@ -39,10 +47,105 @@ void set_smack_free(void (*cb)(void *ptr))
 
 TbResult play_smk_direct(const char *fname, ulong smkflags, ushort plyflags, ushort mode)
 {
+#if 1
     TbResult ret;
     asm volatile ("call ASM_play_smk_direct\n"
         : "=r" (ret) : "a" (fname), "d" (smkflags), "b" (plyflags), "c" (mode));
     return ret;
+#else
+    struct Smack *p_smk;
+    int scr_x, scr_y;
+    int frm_no;
+    TbBool finish, update_pal;
+    uint32_t soflags;
+
+    if ((plyflags & 0x0001) == 0)
+    {
+        struct DIG_DRIVER *p_snddrv;
+
+        p_snddrv = GetSoundDriver();
+        if (p_snddrv != NULL) {
+            SMACKSOUNDUSEMSS(0, p_snddrv);
+        } else {
+            plyflags |= 0x0001;
+        }
+    }
+
+    if ((plyflags & 0x0001) != 0)
+        soflags = 0;
+    else
+        soflags = 0xFE000;
+    p_smk = SMACKOPEN(0xFFFFFFFF, soflags, fname);
+    if (p_smk == NULL) {
+        return Lb_FAIL;
+    }
+
+    if ((plyflags & 0x0100) != 0)
+    {
+#if defined(DOS)||defined(GO32)
+        scr_x = (320 - p_smk->Width) >> 1;
+        scr_y = (200 - p_smk->Height) >> 1;
+        SMACKTOBUFFER(0, 0xA0000, 200, 320, scr_y, scr_x, p_smk);
+#else
+        assert(!"hardware framebuffer access not supported");
+#endif
+    }
+    else
+    {
+        scr_x = (lbDisplay.GraphicsScreenWidth - p_smk->Width) >> 1;
+        scr_y = (lbDisplay.GraphicsScreenHeight - p_smk->Height) >> 1;
+        SMACKTOBUFFER(0, lbDisplay.WScreen, lbDisplay.GraphicsScreenHeight,
+          lbDisplay.GraphicsScreenWidth, scr_y,  scr_x, p_smk);
+    }
+
+    finish = false;
+    for (frm_no = 0; !finish; frm_no++)
+    {
+        if ((plyflags & 0x0400) == 0)
+        {
+            if (frm_no >= p_smk->Frames)
+                break;
+        }
+        if ((plyflags & 0x0100) != 0)
+        {
+            if (p_smk != (struct Smack *)-108)
+            {
+                LbMemoryCopy(byte_1E56DC, p_smk->Palette, PALETTE_8b_SIZE);
+                LbPaletteSet(byte_1E56DC);
+            }
+            SMACKDOFRAME(p_smk);
+        }
+        else
+        {
+            update_pal = false;
+            if (p_smk != (struct Smack *)-108)
+            {
+                update_pal = true;
+                LbMemoryCopy(byte_1E56DC, p_smk->Palette, PALETTE_8b_SIZE);
+            }
+            SMACKDOFRAME(p_smk);
+            if (update_pal)
+            {
+                LbScreenWaitVbi();
+                LbPaletteSet(byte_1E56DC);
+            }
+            LbScreenSwap();
+        }
+        SMACKNEXTFRAME(p_smk);
+
+        while (SMACKWAIT(p_smk))
+        {
+            if ((plyflags & 0x0002) != 0)
+                continue;
+            if (lbKeyOn[KC_ESCAPE] || lbKeyOn[KC_RETURN] || lbKeyOn[KC_SPACE] || lbDisplay.MLeftButton)
+            {
+                finish = true;
+            }
+        }
+    }
+    SMACKCLOSE(p_smk);
+    return Lb_SUCCESS;
+#endif
 }
 
 TbResult play_smk_via_buffer(const char *fname, ulong smkflags, ushort plyflags, SmackDrawCallback callback)
