6a4b4441e14fea4204fbeba0a6ca133762da43d5
mefistotelis <mefistotelis@gmail.com>
2025-12-26 13:53:52 +0100

    syndwarsfx: Do not set transparency in cover_screen_rect_with_raw_file()

--- src/display.c
+++ src/display.c
@@ -271,7 +271,7 @@ TbResult cover_screen_rect_with_raw_file(short x, short y, ushort w, ushort h, c
         return ret;
     }
     LbScreenUnlock();
-    ret = LbScreenSurfaceBlit(&surf, x, y, &srect, SSBlt_TO_SCREEN | SSBlt_TRANSPRN);
+    ret = LbScreenSurfaceBlit(&surf, x, y, &srect, SSBlt_TO_SCREEN);
     LbScreenSurfaceRelease(&surf);
     LbScreenLock();
     return ret;
