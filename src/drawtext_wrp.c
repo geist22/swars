00ca28d2b85be7cd5eb15f2fe543f90e0ebd0186
mefistotelis <mefistotelis@gmail.com>
2026-01-22 14:01:31 +0100

    swrender: Added transformed line drawing

--- src/drawtext_wrp.c
+++ src/drawtext_wrp.c
@@ -24,43 +24,28 @@
 #include "bfscreen.h"
 
 #include "drawtext.h"
+#include "drawshape.h"
+#include "engintrns.h"
+
 #include "bigmap.h"
 #include "display.h"
-#include "engintrns.h"
 #include "swlog.h"
 /******************************************************************************/
 
-void draw_line_transformed_at_ground(int x1, int y1, int x2, int y2, TbPixel colour)
+void draw_line_transformed_at_ground(int x1, int z1, int x2, int z2, TbPixel colour)
 {
 #if 0
     asm volatile (
       "push %4\n"
       "call ASM_draw_line_transformed_at_ground\n"
-        : : "a" (x1), "d" (y1), "b" (x2), "c" (y2), "g" (colour));
+        : : "a" (x1), "d" (z1), "b" (x2), "c" (z2), "g" (colour));
 #endif
-    int alt;
-    struct EnginePoint ep1, ep2;
+    int coord1_y, coord2_y;
 
-    ep1.X3d = x1 - engn_xc;
-    alt = alt_at_point(x1, y1);
-    ep1.Y3d = (alt >> 5);
-    ep1.Z3d = y1 - engn_zc;
-    ep1.Flags = 0;
-    transform_point(&ep1);
+    coord1_y = PRCCOORD_TO_YCOORD(alt_at_point(x1, z1));
+    coord2_y = PRCCOORD_TO_YCOORD(alt_at_point(x2, z2));
 
-    ep2.X3d = x2 - engn_xc;
-    alt = alt_at_point(x2, y2);
-    ep2.Y3d = (alt >> 5) - engn_yc;
-    ep2.Z3d = y2 - engn_zc;
-    ep2.Flags = 0;
-    transform_point(&ep2);
-
-    if ((ep1.pp.X > 0) && (ep1.pp.X < lbDisplay.GraphicsScreenWidth)
-      && (ep1.pp.Y > 0) && (ep1.pp.Y < lbDisplay.GraphicsScreenHeight)
-      && (ep2.pp.X > 0) && (ep2.pp.X < lbDisplay.GraphicsScreenWidth)
-      && (ep2.pp.Y > 0) && (ep2.pp.Y < lbDisplay.GraphicsScreenHeight)) {
-        LbDrawLine(ep1.pp.X, ep1.pp.Y, ep2.pp.X, ep2.pp.Y, colour);
-    }
+    draw_line_transformed_col(x1, coord1_y, z1, x2, coord2_y, z2, colour);
 }
 
 void draw_text_transformed_at_ground(int coord_x, int coord_z, const char *text)
---
+++ swrendersoft/include/drawshape.h
@@ -0,0 +1,41 @@
+/******************************************************************************/
+// Syndicate Wars Fan Expansion, source port of the classic game from Bullfrog.
+/******************************************************************************/
+/** @file drawshape.h
+ *     Header file for drawshape.c.
+ * @par Purpose:
+ *     Drawing simple 2D shapes within the 3D world.
+ * @par Comment:
+ *     Just a header file - #defines, typedefs, function prototypes etc.
+ * @author   Tomasz Lis
+ * @date     19 Apr 2022 - 27 Aug 2023
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef DRAWSHAPE_H
+#define DRAWSHAPE_H
+
+#include "bftypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/******************************************************************************/
+#pragma pack(1)
+
+
+#pragma pack()
+/******************************************************************************/
+
+void draw_line_transformed_col(int x1, int y1, int z1, int x2, int y2, int z2,
+  TbPixel colour);
+
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+#endif
